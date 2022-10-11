809a6591031648b55a2c1c38c316232cb8234763
baumgnoa <baumgnoa@students.zhaw.ch>
2022-10-11 18:29:28 +0200

    split project in different components, added icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { PreviewComponent } from './preview/preview.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    PreviewComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
